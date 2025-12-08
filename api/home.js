079e694af04d642443e6ce8ebee1319ec667d926
ruazp <56259300+ruazp@users.noreply.github.com>
2025-12-08 14:01:02 +0700

    Update API endpoint for home data fetching

--- api/home.js
+++ api/home.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://otakudesu-api.sinkronmedia.com/home');
+    const response = await fetch('https://otakudesu-anime-api.vercel.app');
     const data = await response.json();
     res.setHeader('Access-Control-Allow-Origin', '*'); // Fix CORS!
     res.json(data);
